096356c468d5d77ad03e1da9da6da8df1b97fc5f
giuliarenda <giuliarenda94@gmail.com>
2024-07-02 19:25:18 +0100

    survey page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const groupAFetch = fetch('https://raw.githubusercontent.com/mondoboia/conceptual-saliency/main/a_group_data.json');
     const groupBFetch = fetch('https://raw.githubusercontent.com/mondoboia/conceptual-saliency/main/b_group_data.json');
 
-    let groupAData, groupBData;
+    let groupAData, groupBData, userId;
 
     Promise.all([groupAFetch, groupBFetch])
         .then(responses => Promise.all(responses.map(response => response.json())))
@@ -11,6 +11,20 @@ document.addEventListener('DOMContentLoaded', function () {
             groupBData = data[1];
         })
         .catch(error => console.error('Error loading JSON data:', error));
+
+    document.getElementById('start-survey').addEventListener('click', function () {
+        const expertise = document.getElementById('expertise').value;
+        let firstName = document.getElementById('first-name').value.trim();
+        if (firstName === '') {
+            alert('Please enter your first name.');
+            return;
+        }
+        firstName = firstName.replace(/\s+/g, '-');
+        const sessionId = generateUniqueId();
+        userId = `${expertise}_${firstName}_${sessionId}`;
+        document.getElementById('user-info').style.display = 'none';
+        document.getElementById('group-selection').style.display = 'block';
+    });
 
     document.getElementById('select-group-a').addEventListener('click', () => startSurvey('a', groupAData));
     document.getElementById('select-group-b').addEventListener('click', () => startSurvey('b', groupBData));
@@ -25,7 +39,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const results = {
             group: group,
-            data: []
+            data: [],
+            userId: userId  // Include userId in results
         };
 
         let pairs = [];
@@ -45,7 +60,6 @@ document.addEventListener('DOMContentLoaded', function () {
         function updateProgress() {
             const progressElement = document.getElementById(`progress-${group}`);
             progressElement.textContent = `Question ${questionIndex + 1} of ${pairs.length}`;
-            // progressElement.textContent = `Question ${questionIndex + 1} of 10`;
         }
 
         function createAttributeList(attributes) {
@@ -56,6 +70,40 @@ document.addEventListener('DOMContentLoaded', function () {
                 ul.appendChild(li);
             });
             return ul;
+        }
+
+        function createLikertScale(name, labels) {
+            const scale = document.createElement('div');
+            scale.className = 'likert-scale';
+
+            labels.forEach((label, index) => {
+                const inputLabel = document.createElement('label');
+                inputLabel.innerHTML = `
+                    <input type="radio" name="${name}" value="${index + 1}">
+                    <br>
+                    ${label}
+                `;
+                scale.appendChild(inputLabel);
+            });
+
+            return scale;
+        }
+
+        function getSaliencyValue(value, leftConcept, rightConcept) {
+            switch (value) {
+                case '1':
+                    return { saliency: `strong ${leftConcept.saliency}`, conceptId: leftConcept.id };
+                case '2':
+                    return { saliency: `weak ${leftConcept.saliency}`, conceptId: leftConcept.id };
+                case '3':
+                    return { saliency: 'none', conceptId: 0 };
+                case '4':
+                    return { saliency: `weak ${rightConcept.saliency}`, conceptId: rightConcept.id };
+                case '5':
+                    return { saliency: `strong ${rightConcept.saliency}`, conceptId: rightConcept.id };
+                default:
+                    return { saliency: 'none', conceptId: 0 };
+            }
         }
 
         function createQuestion(method, chart, pair) {
@@ -70,29 +118,31 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div class="attribute-wrapper">
                         <strong>Left:</strong>
                         <div class="attribute-container"></div>
+                        <p>Artworks: ${pair[0].artworks_num}</p>
+                        <p>Percentage: ${pair[0].artworks_percentage}%</p>
                     </div>
                     <div class="attribute-wrapper">
                         <strong>Right:</strong>
                         <div class="attribute-container"></div>
+                        <p>Artworks: ${pair[1].artworks_num}</p>
+                        <p>Percentage: ${pair[1].artworks_percentage}%</p>
                     </div>
                 </div>
                 <div class="question-group">
                     <p>Which concept is more <b>representative</b>?</p>
-                    <label>
-                        <input type="radio" name="rep" value="left"> Left
-                    </label>
-                    <label>
-                        <input type="radio" name="rep" value="right"> Right
-                    </label>
+                </div>
+                <div class="likert-wrapper">
+                    <div class="likert-scale">
+                        ${createLikertScale('rep', ['Strongly Left', 'Left', 'Neutral', 'Right', 'Strongly Right']).outerHTML}
+                    </div>
                 </div>
                 <div class="question-group">
-                    <p>Which concept is more <b>peculiar</b>?</p>
-                    <label>
-                        <input type="radio" name="pec" value="left"> Left
-                    </label>
-                    <label>
-                        <input type="radio" name="pec" value="right"> Right
-                    </label>
+                    <p>Which concept is more <b>prominent</b>?</p>
+                </div>
+                <div class="likert-wrapper">
+                    <div class="likert-scale">
+                        ${createLikertScale('pec', ['Strongly Left', 'Left', 'Neutral', 'Right', 'Strongly Right']).outerHTML}
+                    </div>
                 </div>
                 <button id="next-button" disabled>Next</button>
             `;
@@ -120,22 +170,28 @@ document.addEventListener('DOMContentLoaded', function () {
                 const repValue = questionDiv.querySelector('input[name="rep"]:checked').value;
                 const pecValue = questionDiv.querySelector('input[name="pec"]:checked').value;
 
-                const selectedRep = repValue === 'left' ? pair[0] : pair[1];
-                const selectedPec = pecValue === 'left' ? pair[0] : pair[1];
+                const repSaliency = getSaliencyValue(repValue, pair[0], pair[1]);
+                const pecSaliency = getSaliencyValue(pecValue, pair[0], pair[1]);
 
-                results.data.push({
-                    questionNumber: questionIndex,
+                const currentResult = {
+                    questionNumber: questionIndex + 1,
                     timestamp: new Date().toISOString(),
-                    id: generateUniqueId(),
                     group: group,
                     method: method,
                     chart: chart,
-                    rep: selectedRep.id,
-                    repSaliency: selectedRep.saliency,
-                    pec: selectedPec.id,
-                    pecSaliency: selectedPec.saliency
-                });
+                    rep: repSaliency.conceptId,
+                    repSaliency: repSaliency.saliency,
+                    pec: pecSaliency.conceptId,
+                    pecSaliency: pecSaliency.saliency,
+                    userId: userId // Include userId
+                };
 
+                results.data.push(currentResult);
+
+                // Send current result to server
+                sendResult(currentResult);
+
+                questionIndex++; // Increment after capturing the result
                 nextQuestion();
             });
 
@@ -152,19 +208,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Survey is complete
                 document.getElementById(`survey-group-${group}`).style.display = 'none';
                 document.getElementById('thank-you').style.display = 'block';
-                console.log('Survey results:', results); // Log the results for testing
-                sendResults(results); // Send the results to the server for testing
+                console.log('Survey results:', results);
                 return;
             }
-
-            // if (questionIndex >= 10 || questionIndex >= pairs.length) {
-            //     // Survey is complete after 10 questions
-            //     document.getElementById(`survey-group-${group}`).style.display = 'none';
-            //     document.getElementById('thank-you').style.display = 'block';
-            //     console.log('Survey results:', results); // Log the results for testing
-            //     sendResults(results); // Send the results to the server for testing
-            //     return;
-            // }
 
             updateProgress();
 
@@ -172,22 +218,21 @@ document.addEventListener('DOMContentLoaded', function () {
             const question = createQuestion(method, chart, pair);
 
             container.appendChild(question);
-            questionIndex++;
         }
 
         nextQuestion();
     }
 
-
-    function sendResults(results) {
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbxYackKUPibBYz3mGFZ4JNZYGHFNzU3k93pcgh6dLfgORg0I_RGmzVsrvjeRGfE4oIkmQ/exec';
+    function sendResult(result) {
+        console.log('Sending result:', JSON.stringify(result));
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbxbjZkZioMUiCXIHYMdIjFYvuWOotBzUqp64LoHZdoYtd015LMJKXCsQVmCxpjP6stIMw/exec';
         fetch(scriptURL, {
             method: 'POST',
             mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ results: results })
+            body: JSON.stringify(result)
         })
             .then(response => console.log('Success:', response))
             .catch(error => console.error('Error:', error));
